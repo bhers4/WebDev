2e9cb39b4e73cf6510669017c6d959f54d2f1927
Ben Hers <bhers4@gmail.com>
2018-12-19 16:02:34 -0800

    Finished Color Game

--- JS/ColorGame/app.js
+++ JS/ColorGame/app.js
@@ -1,5 +1,8 @@
 var colors = generatecolors(6);
+
 var squares = document.querySelectorAll(".square");
+var exsquares = document.querySelectorAll(".extremesquare");
+
 var pickedColor = colors[pickColor()];
 var rgbdisplay = document.querySelector("#rgbdisplay");
 var messagedisplay = document.querySelector("#message");
@@ -7,16 +10,103 @@ var headerdisplay = document.querySelector(".headergame");
 var newcolorsbutt = document.querySelector("#NewColors");
 var default_color = "#232323";
 var temp_color = 0;
+var easybutton = document.querySelector("#easy");
+var hardbutton = document.querySelector("#hard");
+var extrahardbutton = document.querySelector("#extrahard");
+var mode = "hard";
+
+easybutton.addEventListener("click",function(){
+    if (mode!="easy")
+    {
+        mode = "easy";
+        easybutton.classList.add('selected');
+        hardbutton.classList.remove('selected');
+        extrahardbutton.classList.remove('selected');
+        colors = generatecolors(3);
+        pickedColor = colors[pickColor()];
+        rgbdisplay.textContent = pickedColor;
+        for (var i=0;i<squares.length;i++)
+        {
+            if(colors[i])
+            {
+                squares[i].style.backgroundColor = colors[i];
+            }
+            else
+            {
+                squares[i].style.display = "None";
+            }
+        }
+    }
+    
+});
+
+hardbutton.addEventListener("click",function(){
+    if (mode!="hard")
+    {
+        mode = "hard";
+        easybutton.classList.remove('selected');
+        hardbutton.classList.add('selected');
+        extrahardbutton.classList.remove('selected');
+        colors = generatecolors(6);
+        pickedColor = colors[pickColor()];
+        rgbdisplay.textContent = pickedColor;
+        for (var i=0;i<squares.length;i++)
+        {
+            if(colors[i])
+            {
+                squares[i].style.backgroundColor = colors[i];
+                squares[i].style.display = "block";
+            }
+            else
+            {
+                squares[i].style.display = "None";
+            }
+        } 
+    }
+});
+
+extrahardbutton.addEventListener("click",function(){
+    if (mode!="extrahard")
+    {
+        mode = "extrahard";
+        easybutton.classList.remove('selected');
+        hardbutton.classList.remove('selected');
+        extrahardbutton.classList.add('selected');
+        colors = generatecolors(9);
+        pickedColor = colors[pickColor()];
+        rgbdisplay.textContent = pickedColor;
+        for (var i=0;i<squares.length;i++)
+        {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+        }
+    }
+});
 
 rgbdisplay.textContent = pickedColor;
 newcolorsbutt.addEventListener("click",function(){
-   reset_all(6);
+   if (mode=="hard")
+    {
+        reset_all(6);
+    }
+    else if (mode=="easy")
+    {
+        reset_all(3);
+    }
+    else
+    {
+        reset_all(9);
+    }
 });
 
 for(var i=0;i<squares.length;i++)
 {
     // Add initial colors
-    squares[i].style.backgroundColor = colors[i];
+    if(colors[i])
+    {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
     // Add click listeners
     squares[i].addEventListener("click",function() {
         var clickedcolor = this.style.backgroundColor;
@@ -81,7 +171,7 @@ function reset_all(num)
     temp_color = pickColor();
     pickedColor = colors[temp_color];
     rgbdisplay.textContent = pickedColor;
-    headerdisplay.style.backgroundColor = default_color;
+    headerdisplay.style.backgroundColor = "steelblue";
     newcolorsbutt.textContent = "New Colors";
     messagedisplay.textContent = "Pick a Color";
 }
